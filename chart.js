47a3917c480249d3979216b8015fc912c35157a2
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-02-18 18:19:44 -0500

    Radar chart added

--- chart.js
+++ chart.js
@@ -1,9 +1,9 @@
 // the answers to the question (passed to line 15)
-let labels1 = ['YES', 'Yes, but in green'];
+let labels1 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 // the values of the answers (passed to line 17)
-let data1 = [69, 31];
+let data1 = [30, 10, 62, 15, 5, 97, 25];
 // data representation colors (passed to line 18)
-let colors1 = ['#49A9EA', '#36CAAB'];
+let colors1 = ['#FCD5BE;', '#F8B195', '#F67280', '#C06C84', '#A8A0B1', '#6C5B7B', '#355C7D'];
 
 // myChart1 = the query in 2D context
 let myChart1 = document.getElementById("myChart").getContext('2d');
@@ -21,7 +21,7 @@ let chart1 = new Chart(myChart1, {
     options: {
         // title = the question we are asking
         title: {
-            text: "Do you like to practice?",
+            text: "How many minutes did your practice this week?",
             display: true
         }
     }
@@ -58,5 +58,52 @@ let chart = new Chart(myChart2, {
         legend: {
             display: false
         }
+    }
+});
+
+
+//________________________________________________________________________________________
+
+let labels3 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// the values of the answers (passed to line 17)
+// let data3 = [30, 10, 62, 15, 5, 97, 25];
+// data representation colors (passed to line 18)
+// let colors3 = ['#FCD5BE;', '#F8B195', '#F67280', '#C06C84', '#A8A0B1', '#6C5B7B', '#355C7D'];
+
+// myChart1 = the query in 2D context
+let myChart3 = document.getElementById("myChart3").getContext('2d');
+
+// renders the chart
+let chart3 = new Chart(myChart3, {
+    type: 'radar',
+    data: {
+        labels: labels3, 
+        datasets: [
+            {
+                label: 'Student week 1',
+                fill: true,
+                backgroundColor: "rgba(179, 181, 198, 0.2)", 
+                borderColor: "rgba(179, 181, 198, 1)",
+                pointBorderColor: "#FFF",
+                pointBackgroundColor: "rgba(179, 181, 198, 1)",
+                data: [30, 10, 62, 15, 5, 97, 25]
+            },
+            {
+                label: 'Student week 2',
+                fill: true,
+                backgroundColor: "rgba(255, 99, 132, 0.2)", 
+                borderColor: "rgba(255, 99, 132, 1)",
+                pointBorderColor: "#FFF",
+                pointBackgroundColor: "rgba(255, 99, 132, 1)",
+                data: [50, 0, 12, 25, 0, 160, 30]
+            },
+        ] 
+    },
+    options: {
+        // title = the question we are asking
+        title: {
+            text: "Minutes of practice day this week and last week",
+            display: true
+        },
     }
 });
